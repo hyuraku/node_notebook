d2c07574d4620e7ab351a40237460cbe0cfd3f5f
hyuraku <yu@hill-wood.com>
2019-05-10 23:37:32 +0900

    start to write good tips

---
+++ good_tips.js
@@ -0,0 +1,6 @@
+const foo = {name: 'Tom',age: 35, nervous: false}
+const bar = {name: 'Tim',age: 45, nervous: true}
+const baz = {name: 'Mark',age: 25, nervous: false}
+
+console.log({foo, bar, baz})
+console.table([foo,bar,baz])
