f2dbdd46c0a84174083b4d8a922cafa03a49a5fc
hyuraku <yu@hill-wood.com>
2019-05-11 14:56:34 +0900

    add tips about destructring

--- good_tips.js
+++ good_tips.js
@@ -1,6 +1,38 @@
+// console.log
+
 const foo = {name: 'Tom',age: 35, nervous: false}
 const bar = {name: 'Tim',age: 45, nervous: true}
 const baz = {name: 'Mark',age: 25, nervous: false}
 
+console.log('%c my friend', "color: blue;") // show 'my friend' in blur 
 console.log({foo, bar, baz})
 console.table([foo,bar,baz])
+
+// Stack Trace log
+const deleteMe = () => console.trace('delete one data')
+
+deleteMe()
+deleteMe()
+
+
+// destructring
+const student = {
+  name: 'Tim',
+  age: 18,
+  glasses: true,
+  height: 180,
+  sport: 'Baseball'
+}
+
+const introduction = ({name, age, sport}) => {
+  return `I am ${name} and ${age} years old, I like ${sport}`
+}
+// OR
+const introduction = (person) => {
+  const {name, age, sport} = person
+  return `I am ${name} and ${age} years old, I like ${sport}`
+}
+
+console.log(introduction(student))
+
+
