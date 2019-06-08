f18172dcce03611f17055f23dee36c3515fd8419
hyuraku <32809703+hyuraku@users.noreply.github.com>
2019-06-08 14:24:58 +0900

    add async and await

--- good_tips.js
+++ good_tips.js
@@ -1,67 +1,121 @@
-// console.log
 
-const foo = {name: 'Tom',age: 35, nervous: false}
-const bar = {name: 'Tim',age: 45, nervous: true}
-const baz = {name: 'Mark',age: 25, nervous: false}
-
-console.log('%c my friend', "color: blue;") // show 'my friend' in blur 
-console.log({foo, bar, baz})
-
-console.table([foo,bar,baz])
-
-// Stack Trace log
-const deleteMe = () => console.trace('delete one data')
-
-deleteMe()
-deleteMe()
+exports.handler = async (event) => {
+  // TODO implement
+  const response = {
+      statusCode: 200,
+      body: JSON.stringify('Hello from Lambda!'),
+  };
+  return response;
+};
 
 
-// destructring
-const student_1 = {
-  name: 'Tim',
-  age: 18,
-  glasses: true,
-  height: 180,
-  sport: 'Baseball'
-}
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB({
+  apiVersion: '2012-08-10'
+});
 
-const introduction_1 = ({name, age, sport}) => {
-  return `I am ${name} and ${age} years old, I like ${sport}`
-}
-// OR
-const introduction_2 = (person) => {
-  const {name, age, sport} = person
-  return `I am ${name} and ${age} years old, I like ${sport}`
-}
 
-console.log(introduction_1(student_1))
-console.log(introduction_2(student_1))
+exports.handler = async (event) => {
+  const type = event.type;
+  if (type == 'all') {
+    const params = {
+      TableName: 'compare-yourself'
+    };
+    try{
+      const data = await dynamodb.scan(params).promise();
+      const items = data.Items.map(
+        (dataField) => {
+          return {
+            age: +dataField.Age.N,
+            height: +dataField.Height.N,
+            income: +dataField.Income.N
+          };
+        }
+      );
+      return items
+    }catch{
+      console.log(err);
+      return err;
+    }
+  } else if (type == 'single') {
+    const params = {
+      Key: {
+        "UserId": {
+          S: "user_0.012634276781234854"
+        }
+      },
+      TableName: "compare-yourself"
+    };
+    try{
 
-// template-literals
-const student_2 = {
-  name: 'Tim',
-  age: 18,
-  favorite: ['language', 'math'],
-  height: 180
-}
+    }catch{
+      console.log(err);
+      return err;
+    }
+    dynamodb.getItem(params), function(err, data) {
+      if (err) {
+        
+      } else {
+        console.log(data);
+        callback(null, [{
+          age: +data.Item.Age.N,
+          height: +data.Item.Height.N,
+          income: +data.Item.Income.N
+        }]);
+      }
+    });
+  } else {
+    return 'Something went wrong!';
+  }
+};
 
-const {name, age, favorite} = student_2
-let intro = `He is ${name} and ${age} years old, he is good at ${favorite.join(' and ')}`
-console.log(intro)
-
-const aboutAge = (str,age) => {
-  const ageStr = age > 20 ? 'old' : 'young';
-  return `${str[0]}${ageStr} at ${age} years`
-}
-
-const intro_2 = aboutAge`He is ${student_2.age}`
-console.log(intro_2)
-
-// sperad-syntax
-const student_3 = {name: 'Tom'}
-const result = {language: 80, math: 90, science: 75}
-
-const student_3_result = {...student_3, ...result}
-const student_3_result2 = {...student_3, history: 60}
-console.log(student_3_result)
-student_3_result2
+exports.handler = (event, context, callback) => {
+  const type = event.type;
+  if (type == 'all') {
+    const params = {
+      TableName: 'compare-yourself'
+    };
+    dynamodb.scan(params, function(err, data) {
+      if (err) {
+        console.log(err);
+        callback(err);
+      } else {
+        console.log(data);
+        const items = data.Items.map(
+          (dataField) => {
+            return {
+              age: +dataField.Age.N,
+              height: +dataField.Height.N,
+              income: +dataField.Income.N
+            };
+          }
+        );
+        callback(null, items);
+      }
+    });
+  } else if (type == 'single') {
+    const params = {
+      Key: {
+        "UserId": {
+          S: "user_0.012634276781234854"
+        }
+      },
+      TableName: "compare-yourself"
+    };
+    dynamodb.getItem(params, function(err, data) {
+      if (err) {
+        console.log(err);
+        callback(err);
+      } else {
+        console.log(data);
+        callback(null, [{
+          age: +data.Item.Age.N,
+          height: +data.Item.Height.N,
+          income: +data.Item.Income.N
+        }]);
+      }
+    });
+  } else {
+    callback('Something went wrong!');
+  }
+};
