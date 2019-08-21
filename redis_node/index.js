6b7fb02760042f423bc94d2b03c864b4df731037
hyuraku <32809703+hyuraku@users.noreply.github.com>
2019-08-21 23:37:56 +0900

    access github

---
+++ redis_node/index.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const fetch = require("node-fetch")
+const redis = require("redis")
+
+const PORT = process.env.PORT || 5000
+const REDIS_PORT = process.env.PORT || 6379
+
+const client = redis.createClient(REDIS_PORT)
+
+const app = express();
+
+// make request to Github for data
+async function getRepos(req, res, next){
+  try{
+    console.log('Fetching Data...');
+    const { username } = req.params;
+
+    const response = await fetch(`https://api.github.com/users/${username}`);
+
+    const data = await response.json();
+
+    res.send(data)
+  }catch(err){
+    console.log(err);
+    res.status(500);
+  }
+}
+
+app.get('/repos/:username', getRepos);
+
+app.listen(5000, ()=>{
+  console.log(`App listing on port ${PORT}`)
+});
